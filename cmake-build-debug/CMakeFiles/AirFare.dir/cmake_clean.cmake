391522e98b94faa52dd6e06641b5298623d7b41a
shiglar <43323407+shiglar@users.noreply.github.com>
2021-04-17 17:28:00 -0600

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/AirFare.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "AirFare.exe"
+  "AirFare.exe.manifest"
+  "AirFare.pdb"
+  "CMakeFiles/AirFare.dir/main.cpp.obj"
+  "libAirFare.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AirFare.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
